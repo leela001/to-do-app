dcfad50e922fdb17080cf8963d7297ddb3b7c405
Leelapavan <leelapavankumarchitirala25@gmail.com>
2024-04-26 20:00:04 +0530

    Handling the creation of new 'To Do' status task when existing todos exceed 50% of total tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,15 +4,26 @@ class Task < ApplicationRecord
 
 
     def self.save_task(params)
-        debugger
         if params["id"].present?
             task = Task.find_by_id(params["id"])
             raise Exception.new("In valid Task ID: #{params["id"]}")
         else
-            task = Task.new
+            if params["status"] == 'To Do'
+                total_tasks = Task.all.count
+                to_dos_count = Task.where("status = 'To Do'").count
+                if to_dos_count.to_f/total_tasks.to_f * 100 >= 50
+                    raise Exception.new("Cannot create more 'To Do' tasks. Limit reached")
+                else
+                    task = Task.new
+                    task.description = params["description"]
+                    task.title = params["title"]
+                end
+            else
+                task = Task.new
+                task.description = params["description"]
+                task.title = params["title"]
+            end
         end
-        task.title = params["title"]
-        task.description = params["description"]
         task.status = params["status"]
         task.save!
         return task
